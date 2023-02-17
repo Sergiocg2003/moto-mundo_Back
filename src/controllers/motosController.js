09924afd186db81dcbb9e0adbf32e2a125be87f6
Sergio <scorgon265@g.educaand.es>
2023-02-17 12:03:48 +0100

    correccion de errores

--- src/controllers/motosController.js
+++ src/controllers/motosController.js
@@ -2,9 +2,9 @@ const motosServices = require("../services/motosServices.js");
 
 //  /api/v1/motos
 const getAllMotos = (req, res, next) => {
-    const allPMotos = motosServices.getAllMotos();
+    const allMotos = motosServices.getAllMotos();
   
-    if (!Object.keys(allPMotos).length === 0) {
+    if (!Object.keys(allMotos).length === 0) {
        res.send(allMotos);
     } else {
        res.status(404).end();
